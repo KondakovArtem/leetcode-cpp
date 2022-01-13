3e545fab25d70c4055d7f94755a22b215f0552c5
Kondakov Artem <kondakov.artem@gmail.com>
2022-01-13 21:36:42 +0300

    add-binary solved

--- src/add-binary.hpp
+++ src/add-binary.hpp
@@ -9,19 +9,26 @@ using namespace std;
 class Solution
 {
 public:
+
+    char getBit(char n, char k) {
+        return (n & ( 1 << k )) >> k;
+    }
+
     string addBinary(string a, string b)
     {
-        a.pop_back()
-        
-        int nA = stoi(a, 0, 2);
-        int nB = stoi(b, 0, 2);
-        int nRes = nA + nB;
+        const auto aL = a.length();
+        const auto bL = b.length();
+        const auto maxL = max(aL, bL);
         string res = "";
-        do
-        {
-            res.insert(0, nRes & 1 ? "1" : "0");
-            nRes = nRes >> 1;
-        } while (nRes != 0);
+        char carry = 0;
+        for (int i=0; i<maxL; i++) {
+            const char bA = ((i <= aL - 1 ) && a[aL - 1 - i] == '1') || 0;
+            const char bB = ((i <= bL - 1 ) && b[bL - 1 - i] == '1') || 0;
+            const char sum = bA + bB + carry;
+            carry = getBit(sum, 1);
+            res.insert(0, getBit(sum, 0) ? "1" : "0");
+        }
+        if (carry) res.insert(0, "1");
         return res;
     }
 };
